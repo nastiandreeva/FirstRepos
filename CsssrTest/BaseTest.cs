e54b40bf04da010a38043080f79b612195ba358d
Андреева Анастасия Олеговна <aandreeva@gems.local>
2020-02-07 14:36:38 +0600

    Open link in new window

--- CsssrTest/BaseTest.cs
+++ CsssrTest/BaseTest.cs
@@ -26,13 +26,31 @@ namespace CsssrTest
 			var targetSoft = "//label[@for='soft']";
 			_driver.FindElement(By.XPath(targetSoft)).Click();
 
-			var target = "http://monosnap.com/"; //правильная ссылка по ТЗ
-			//var target = "http://app.prntscr.com/ru/"; //не корректная ссылка указанная в коде страницы
+			//var target = "http://monosnap.com/"; //правильная ссылка по ТЗ
+			var target = "http://app.prntscr.com/ru/"; //не корректная ссылка указанная в коде страницы
 			Assert.Equal(target, _driver.FindElement(By.XPath("//a[text()='Софт для быстрого создания скриншотов']")).GetAttribute("href"));
+
+			//проверка что ссылка на софт открывается в новом окне
+			var targetNewWin = "_blank";
+			Assert.Equal(targetNewWin, _driver.FindElement(By.XPath("//a[text()='Софт для быстрого создания скриншотов']")).GetAttribute("target"));
 
 			_driver.Quit();
 		}
 
+		[Fact]
+		public static void TargetVk()
+		{
+			/*var targetVk = "//a[@href='http://vk.com/csssr']"; //кликабельность вк
+			Assert.True(_driver.FindElements(By.XPath(targetVk).));
+			*/
+		}
 
+		[Fact]
+		public static void TargetMail()
+		{
+			/*var targetEmail = "//a[@href='mailto:hr@csssr.com']"; //кликабельность почты
+			Assert.Equal(targetEmail, _driver.FindElement(By.XPath("//a[text()='Софт для быстрого создания скриншотов']")).GetAttribute("href"));
+			*/
+		}
 	}
 }
